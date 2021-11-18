3106efbb91ac02a07e09aa452a2ef26f4fa2cd47
Salih Gumus <sudanmerinosu@gmail.com>
2021-11-18 17:39:56 +0300

    Base template

---
+++ src/components/Example.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="example">
+		<h1>{{ msg }}</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'example',
+	props: {
+		msg: String,
+	},
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Navbar/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<nav class="navbar">
+		<h1>Navbar</h1>
+	</nav>
+</template>
+
+<script>
+export default {
+	name: 'navbar',
+};
+</script>
+
+<style scoped></style>
