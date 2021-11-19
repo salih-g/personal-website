27cf937b36e2673b4316427a81c8e428285812ee
Salih Gumus <sudanmerinosu@gmail.com>
2021-11-19 14:03:42 +0300

    Navbar changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,21 @@
 <template>
 	<nav class="navbar">
-		<h3 class="title">
+		<span class="title">
 			<a href="/" class="link">{{ title }} </a>
-		</h3>
+		</span>
+		<a
+			href="https://github.com/sudanmerinosu/"
+			target="_blank"
+			class="link hover"
+		>
+			Works
+		</a>
+		<a
+			href="https://github.com/sudanmerinosu/personal-website"
+			target="_blank"
+			class="link hover"
+			><i class="fab fa-github"></i> Source
+		</a>
 	</nav>
 </template>
 
@@ -19,9 +32,17 @@ export default {
 .navbar {
 	height: 56px;
 	margin: 0 auto;
+	margin-top: -30px;
+}
+.navbar a {
+	margin-right: 40px;
 }
 .title {
 	font-weight: bold;
 	font-size: 18px;
+	margin-right: 10px;
+}
+.hover:hover {
+	text-decoration: underline;
 }
 </style>
---
+++ src/components/Web.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="main">
+		<h3 class="title">On the Web</h3>
+		<p>
+			{{ hobys }}
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Love',
+	props: {
+		hobys: String,
+	},
+};
+</script>
+
+<style scoped>
+.main {
+	margin-top: 50px;
+}
+.title {
+	border-bottom: 5px solid #525252;
+	width: 35px;
+}
+</style>
